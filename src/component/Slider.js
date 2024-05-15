491e047cd04f57f093a7505246eb80257bcf497d
anas <ashiwani007@gmail.com>
2024-05-15 17:59:07 +0500

    Complete

---
+++ src/component/Slider.js
@@ -0,0 +1,36 @@
+import '../css/Slider.css'
+import img from '../assets/bed image.webp'
+import img2 from '../assets/lamp image.webp'
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css'
+
+function Slider() {
+    return (
+        <>
+            <div className="slider-main">
+                {/* <div className='container'>
+                    <div className='row'>
+                        <div className='col-sm-12 col-lg-12 col-md-12 col-12 '>
+                            <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active" data-bs-interval="10000">
+                                        <img src={img} class="d-block " alt="..." style={{height:'80vh', width:'100vw'}}></img>
+                                    </div>
+                                    <div className="carousel-item" data-bs-interval="2000">
+                                        <img src={img2}className="d-block " alt="..."></img>
+                                    </div>
+                                  
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+
+
+               
+            </div>
+        </>
+    )
+}
+export default Slider;
